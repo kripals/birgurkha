40709cce1e7c6685cffc8700da0c2ba411de0467
Kripal <kripal@sastodeal.com>
2020-05-06 18:33:59 +0545

    add the locals table

---
+++ database/migrations/2020_05_06_123942_create_locals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('entity_id');
+            $table->enum('magento_type', ['PRODUCT', 'CATEGORY'])->default('PRODUCT');
+            $table->string('name');
+            $table->enum('type', ['SLIDER','CATEGORY','PRODUCT'])->default('SLIDER');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locals');
+    }
+}
