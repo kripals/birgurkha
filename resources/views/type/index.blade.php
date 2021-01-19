2f977b69e72b875631695d093bce4ef465844b1d
KripalSth <kripal@sastodeal.com>
2021-01-19 12:26:58 +0545

    create type

--- resources/views/type/index.blade.php
+++ resources/views/type/index.blade.php
@@ -8,12 +8,12 @@
             <div class="card">
                 <div class="card-head">
                     <header class="text-capitalize">all Homepage Sections</header>
-{{--                    <div class="tools">--}}
-{{--                        <a class="btn btn-primary" href="{{ route('types.create') }}">--}}
-{{--                            <i class="md md-add"></i>--}}
-{{--                            Add--}}
-{{--                        </a>--}}
-{{--                    </div>--}}
+                    <div class="tools">
+                        <a class="btn btn-primary" href="{{ route('types.create') }}">
+                            <i class="md md-add"></i>
+                            Add
+                        </a>
+                    </div>
                 </div>
                 <div class="card-body">
                     <table class="table table-hover">
